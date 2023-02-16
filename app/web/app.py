498434171a9e1c755ecf62f7a335a6ac31a039e9
Yana Sirina <yanasirina0@yandex.ru>
2023-02-17 01:54:45 +0500

    валидация

--- app/web/app.py
+++ app/web/app.py
@@ -2,10 +2,12 @@ from typing import Optional
 
 from aiohttp.web import Application as AiohttpApplication, run_app as aiohttp_run_server, View as AiohttpView, \
     Request as AiohttpRequest
+from aiohttp_apispec import setup_aiohttp_apispec
 
 from app.store.crm.accessor import CrmAccessor
 from app.web.routes import setup_routes
 from app.store import setup_accessors
+from app.web.middlewares import setup_middlewares
 
 
 class Application(AiohttpApplication):
@@ -30,5 +32,7 @@ app = Application()
 
 def run_app():
     setup_routes(app)
+    setup_aiohttp_apispec(app, title='CRM Application', url='/docs/json', swagger_path='/docs')
+    setup_middlewares(app)
     setup_accessors(app)
     aiohttp_run_server(app)
